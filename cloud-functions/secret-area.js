7c44a6b70bb26564894819c56925a90bd5270605
Aroa Cain <aroacain@gmail.com>
2022-04-25 17:42:54 +0200

    Update cloud function to check for a pretend password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,31 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area"
-  })
+
+  // Content to be sent when receiving correct response
+  const secretContent = `
+  <h3>Welcome To The Secret Area</h3>
+  <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+  `
+  
+  let body
+  //Check if the body has any value attacked as a post request
+  if (event.body) {
+    //Parse the response
+    body = JSON.parse(event.body)
+  } else {
+    body = {}
+  }
+
+  // Check if the password is correct
+  if (body.pass == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent
+    })
+  } else {
+    callback(null, {
+      statusCode: 401
+    })
+  }
+
+  
 }
